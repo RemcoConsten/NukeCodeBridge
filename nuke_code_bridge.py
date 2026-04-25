a2545e4c09337cf15040d7a353016b3f03a2f587
RemcoConaten <35838427+RemcoConsten@users.noreply.github.com>
2026-04-25 16:42:09 +0200

    Update BASE_SHARED_PATH for shared scripts location
    Change BASE_SHARED_PATH to point to the new shared scripts location.

--- nuke_code_bridge.py
+++ nuke_code_bridge.py
@@ -29,7 +29,8 @@ except ImportError:
 # Configuration
 # ----------------------------------------------------------------------
 
-BASE_SHARED_PATH        = r"Y:\\nuke_environment\python\NukeCodeBridge"
+#base shared is the location where the shared scripts per user will be located 
+BASE_SHARED_PATH        = r"X:\\nuke_environment\scripts_sharedlocation"
 SHOW_RUN_CONFIRMATION   = True
 USE_SINGLE_SHARED_FOLDER = False
 ENABLE_BACKUPS          = True
